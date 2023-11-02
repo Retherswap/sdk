40fb3188c94672398f4506e2fc6542b249dcc6c3
Rthswap <knut-a_91@hotmail.com>
2023-11-02 02:17:50 +0100

    Initial commit

---
+++ src/constants.ts
@@ -0,0 +1,55 @@
+import JSBI from 'jsbi'
+
+// exports for external consumption
+export type BigintIsh = JSBI | bigint | string
+
+export enum ChainId {
+  MAINNET = 1,
+  ROPSTEN = 3,
+  RINKEBY = 4,
+  GÖRLI = 5,
+  KOVAN = 42,
+  BITGERT = 32520,
+  DOGE = 2000,
+  DOKEN = 61916,
+  FUSE = 122,
+  RTH = 622277,  
+}
+
+export enum TradeType {
+  EXACT_INPUT,
+  EXACT_OUTPUT
+}
+
+export enum Rounding {
+  ROUND_DOWN,
+  ROUND_HALF_UP,
+  ROUND_UP
+}
+
+export const FACTORY_ADDRESS = '0xe7089BE5CeD3965286FEad908103E3d1f0a71Fe1'
+
+export const INIT_CODE_HASH = '0xb6432b000b8fd0e8cd8db4957dfbbbf721ba917ed9d4eb91e9d0682f0118aa72'
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const TWO = JSBI.BigInt(2)
+export const THREE = JSBI.BigInt(3)
+export const FIVE = JSBI.BigInt(5)
+export const TEN = JSBI.BigInt(10)
+export const _100 = JSBI.BigInt(100)
+export const _997 = JSBI.BigInt(997)
+export const _1000 = JSBI.BigInt(1000)
+
+export enum SolidityType {
+  uint8 = 'uint8',
+  uint256 = 'uint256'
+}
+
+export const SOLIDITY_TYPE_MAXIMA = {
+  [SolidityType.uint8]: JSBI.BigInt('0xff'),
+  [SolidityType.uint256]: JSBI.BigInt('0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+}
---
+++ src/entities/token.ts
@@ -0,0 +1,130 @@
+import invariant from 'tiny-invariant'
+import { ChainId } from '../constants'
+import { validateAndParseAddress } from '../utils'
+import { Currency } from './currency'
+
+/**
+ * Represents an ERC20 token with a unique address and some metadata.
+ */
+export class Token extends Currency {
+  public readonly chainId: ChainId
+  public readonly address: string
+
+  public constructor(chainId: ChainId, address: string, decimals: number, symbol?: string, name?: string) {
+    super(decimals, symbol, name)
+    this.chainId = chainId
+    this.address = validateAndParseAddress(address)
+  }
+
+  /**
+   * Returns true if the two tokens are equivalent, i.e. have the same chainId and address.
+   * @param other other token to compare
+   */
+  public equals(other: Token): boolean {
+    // short circuit on reference equality
+    if (this === other) {
+      return true
+    }
+    return this.chainId === other.chainId && this.address === other.address
+  }
+
+  /**
+   * Returns true if the address of this token sorts before the address of the other token
+   * @param other other token to compare
+   * @throws if the tokens have the same address
+   * @throws if the tokens are on different chains
+   */
+  public sortsBefore(other: Token): boolean {
+    invariant(this.chainId === other.chainId, 'CHAIN_IDS')
+    invariant(this.address !== other.address, 'ADDRESSES')
+    return this.address.toLowerCase() < other.address.toLowerCase()
+  }
+}
+
+/**
+ * Compares two currencies for equality
+ */
+export function currencyEquals(currencyA: Currency, currencyB: Currency): boolean {
+  if (currencyA instanceof Token && currencyB instanceof Token) {
+    return currencyA.equals(currencyB)
+  } else if (currencyA instanceof Token) {
+    return false
+  } else if (currencyB instanceof Token) {
+    return false
+  } else {
+    return currencyA === currencyB
+  }
+}
+
+export const WETH = {
+  [ChainId.MAINNET]: new Token(
+    ChainId.MAINNET,
+    '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+    18,
+    'WETH',
+    'Wrapped Ether'
+  ),
+  [ChainId.ROPSTEN]: new Token(
+    ChainId.ROPSTEN,
+    '0xc778417E063141139Fce010982780140Aa0cD5Ab',
+    18,
+    'WETH',
+    'Wrapped Ether'
+  ),
+  [ChainId.RINKEBY]: new Token(
+    ChainId.RINKEBY,
+    '0xc778417E063141139Fce010982780140Aa0cD5Ab',
+    18,
+    'WETH',
+    'Wrapped Ether'
+  ),
+  [ChainId.GÖRLI]: new Token(
+    ChainId.GÖRLI,
+    '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6',
+    18,
+    'WETH',
+    'Wrapped Ether'
+  ),
+  [ChainId.KOVAN]: new Token(
+    ChainId.KOVAN,
+    '0xd0A1E359811322d97991E03f863a0C30C2cF029C',
+    18,
+    'WETH',
+    'Wrapped Ether'
+  ),
+  [ChainId.BITGERT]: new Token(
+    ChainId.BITGERT,
+    '0x0eb9036cbE0f052386f36170c6b07eF0a0E3f710',
+    18,
+    'WBRISE',
+    'Wrapped Brise'
+  ),
+  [ChainId.DOGE]: new Token(
+    ChainId.DOGE,
+    '0xb7ddc6414bf4f5515b52d8bdd69973ae205ff101',
+    18,
+    'WDOGE',
+    'Wrapped Doge'
+  ),
+  [ChainId.DOKEN]: new Token(
+      ChainId.DOKEN,
+      '0x27b45bCC26e01Ed50B4080A405D1c492FEe89d63',
+      18,
+      'WDKN',
+      'Wrapped DoKEN'
+  ),
+  [ChainId.FUSE]: new Token(
+      ChainId.FUSE,
+      '0x0BE9e53fd7EDaC9F859882AfdDa116645287C629',
+      18,
+      'WFUSE',
+      'Wrapped Fuse'
+  ),
+  [ChainId.RTH]: new Token(
+    ChainId.RTH,
+    '0x0000000000E3448F3b4f0a59cDd084689a8eA3cB',
+    18,
+    'WRTH',
+    'Wrapped Rether'
+)
+}
